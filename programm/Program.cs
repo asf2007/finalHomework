4d44d64e3e4f2e11c6d11a1541c72897a0d5cf46
ASF <leha.flyagin@mail.ru>
2022-10-25 22:45:03 +0300

    programm

---
+++ programm/Program.cs
@@ -0,0 +1,42 @@
+﻿void ArrWriter(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+        {
+            Console.Write($"{arr[i]},");
+        }
+        else
+        {
+            Console.Write($"{arr[i]}");
+        }
+    }
+    Console.Write("]");
+}
+
+string[] FindStringsInArray(string[] arr, int sizeString)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= sizeString) count++;
+    }
+    string[] resultArr = new string[count];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= sizeString)
+        {
+            resultArr[j] = arr[i];
+            j++;
+        }
+    }
+
+    return resultArr;
+}
+string[] arr = { "op", "Moscow", "56", "Podolsk", "opa" };
+
+ArrWriter(arr);
+Console.Write(" -> ");
+ArrWriter(FindStringsInArray(arr, 3));
